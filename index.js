99cea0c226567a77bf243c92767b0700656eba00
rachell <dazzlemevanity@gmail.com>
2022-05-31 18:05:05 -0400

    commit tree da44a504eb69c56c119dc79f8cbb02fa24814f26

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// //when doing an annoymus function const returnFirstTwoDrivers =(drivers) =>{
+//     or const returnFirstTwoDrivers =(function (drivers){
+
+const returnFirstTwoDrivers =(first) =>{
+    console.log(first.slice(0,2)) 
+return (first.slice(0,2))
+};
+ 
+const returnLastTwoDrivers = function (last){
+    return(last.slice(-2)) 
+    }
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function (fareMultiplier){
+return function (number) {
+    return fareMultiplier * (number)
+}
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers,func){
+return func(drivers)
+}
+
+// function ben(x){
+
+//     return x
+// }
